b904118dd1efa4538ff5cc214f046513d7634034
Matthew Schmitt <matthew.schmitt@riskiq.net>
2017-06-02 10:47:04 -0700

    Adding params to config class

--- manifests/config.pp
+++ manifests/config.pp
@@ -10,7 +10,17 @@
 #
 # Ayoub Elhamdani <a.elhamdani90@gmail.com>
 #
-class openswan::config {
+class openswan::config(
+  $nat_traversal            = $openswan::params::nat_traversal,
+  $virtual_private          = $openswan::params::virtual_private,
+  $opportunistic_encryption = $openswan::params::opportunistic_encryption,
+  $protostack               = $openswan::params::protostack,
+  $uniqueids                = $openswan::params::uniqueids,
+  $ipsec_conf               = $openswan::params::ipsec_conf,
+  $ipsec_secrets_conf       = $openswan::params::ipsec_secrets_conf,
+  $connections_dir          = $openswan::params::connections_dir,
+  $secrets_dir              = $openswan::params::secrets_dir
+) {
 
   file { $openswan::ipsec_conf:
     ensure  => file,
